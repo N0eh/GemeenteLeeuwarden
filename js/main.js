8ec378de3d757e587b551b62da2641e97f80f78a
Noah Korthals <noah.korthals@gmail.com>
2020-12-08 11:55:11 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const User = function(firstname, middlename, lastname, dateofbirth) {
+    this.firstname = firstname;
+    this.middlename = middlename;
+    this.lastname = lastname;
+    this.dateofbirth = dateofbirth;
+}
+
+let users = []
+
+function Test() {
+    let inputfirstname = document.getElementById('inputfirstname').value
+
+    
+
+    users.push(new User(inputfirstname, ))
+}
